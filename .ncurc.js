7751056f967f5dcc32fa4661707d96fa42cc01db
Kevin Ross <kevin.ross@alienfast.com>
2025-01-17 08:51:09 -0600

    setting up basics for yarn, prettier, eslint, ts

---
+++ .ncurc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  packageFile: './package.json',
+  packageManager: 'yarn',
+  // target: (name: string, versionRange: SemVer[]) => string
+  // target: (name) => (name === '@react-three/fiber' ? '@rc' : 'latest'),
+}
