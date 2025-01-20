578e4c589f64ddedabefb10121b7446882f62a40
Kevin Ross <kevin.ross@alienfast.com>
2025-01-20 10:19:08 -0600

    switch back to main repo example loaders to try meshopt

--- src/gltfjsx.ts
+++ src/gltfjsx.ts
@@ -8,15 +8,59 @@ import transform from './utils/transform.js'
 // DRACOLoader.getDecoderModule = () => {}
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import { KTX2Loader } from 'three/addons/loaders/KTX2Loader.js'
+import { MeshoptDecoder } from 'three/addons/libs/meshopt_decoder.module.js'
 
 import { transformGltfToJsx } from './utils/transformGltfToJsx.js'
 import { Options } from './types.js'
 import { getFileSize } from './utils/getFileSize.js'
 import { toArrayBuffer } from './utils/toArrayBuffer.js'
+import { FileLoader } from 'three'
+import { NodeLoader } from 'three/webgpu'
+import { fileURLToPath } from 'url'
 
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('three/addons/jsm/libs/draco/gltf/')
+
+const ktx2Loader = new KTX2Loader()
+ktx2Loader.setTranscoderPath('three/addons/jsm/libs/basis/')
+
+//draco difficulties in loading, try meshopt instead
+// @see https://github.com/mrdoob/three.js/issues/26403#issuecomment-2101653981
+// @see https://discourse.threejs.org/t/gltf-meshopt-compression/66244/2
+// gltf-transform meshopt input.glb output.glb
 const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(new DRACOLoader())
+gltfLoader.setDRACOLoader(dracoLoader)
+gltfLoader.setKTX2Loader(ktx2Loader)
+gltfLoader.setMeshoptDecoder(MeshoptDecoder)
+
+// const srcDir = path.dirname(fileURLToPath(import.meta.url))
+
+// const dracoLoader = new DRACOLoader()
+// const fileLoader = new FileLoader()
+// const nodeFileLoader = new NodeLoader()
+
+// const dracoDecoderPath =
+//   'file://' + path.resolve(srcDir, '../node_modules/three/examples/jsm/libs/draco/')
+// console.log('dracoDecoderPath', dracoDecoderPath)
+// ;(dracoLoader as any)._loadLibrary = (url: string, responseType: string) => {
+//   console.log('!!!!!!dracoLoader._loadLibrary', url, responseType)
+
+//   const loader = new NodeLoader(dracoLoader.manager)
+//   loader.setPath(dracoDecoderPath)
+//   // loader.setResponseType(responseType)
+//   loader.setWithCredentials(dracoLoader.withCredentials)
+
+//   return new Promise((resolve, reject) => {
+//     loader.load(url, resolve, undefined, reject)
+//   })
+// }
+// dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/') // hallucination?
+// dracoLoader.setDecoderPath(
+//   'file:///Users/kross/projects/gltfjsx/node_modules/three/examples/jsm/libs/draco/',
+// )
+// gltfLoader.setDRACOLoader(dracoLoader)
 
 export default function glftsx(file: string, outputPath: string, options: Options) {
   function getRelativeFilePath(file: string) {
