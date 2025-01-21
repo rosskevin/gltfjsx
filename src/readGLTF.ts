a657fffad9a009b9b54a9e2099a03fdaf9ec47db
Kevin Ross <kevin.ross@alienfast.com>
2025-01-21 14:45:18 -0600

    provide reference load path for model resources

--- src/readGLTF.ts
+++ src/readGLTF.ts
@@ -1,5 +1,7 @@
 import 'global-jsdom/register' // needed for `self` to be defined aka window.self
 
+import path from 'node:path'
+
 import { MeshoptDecoder } from 'three/addons/libs/meshopt_decoder.module.js'
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 import { KTX2Loader } from 'three/addons/loaders/KTX2Loader.js'
@@ -10,31 +12,37 @@ import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 import { readFileToArrayBuffer } from './utils/files.js'
 
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('three/addons/jsm/libs/draco/gltf/')
-
-const ktx2Loader = new KTX2Loader()
-ktx2Loader.setTranscoderPath('three/addons/jsm/libs/basis/')
-
-//draco difficulties in loading, try meshopt instead
-// @see https://github.com/mrdoob/three.js/issues/26403#issuecomment-2101653981
-// @see https://discourse.threejs.org/t/gltf-meshopt-compression/66244/2
-// gltf-transform meshopt input.glb output.glb
-const gltfLoader = new GLTFLoader()
-// gltfLoader.setDRACOLoader(dracoLoader)  // https://github.com/mrdoob/three.js/issues/26403#issuecomment-2101653981
-gltfLoader.setKTX2Loader(ktx2Loader)
-gltfLoader.setMeshoptDecoder(MeshoptDecoder)
-
 /**
  * Read a GLTF file and return the GLTF object.
  */
 export async function readGLTF(modelFilename: string): Promise<GLTF> {
   const modelBuffer = readFileToArrayBuffer(modelFilename)
+
+  //
+  // Setup loader
+  //
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath('three/addons/jsm/libs/draco/gltf/')
+
+  const ktx2Loader = new KTX2Loader()
+  ktx2Loader.setTranscoderPath('three/addons/jsm/libs/basis/')
+
+  //draco difficulties in loading, try meshopt instead
+  // @see https://github.com/mrdoob/three.js/issues/26403#issuecomment-2101653981
+  // @see https://discourse.threejs.org/t/gltf-meshopt-compression/66244/2
+  // gltf-transform meshopt input.glb output.glb
+  const gltfLoader = new GLTFLoader()
+  // gltfLoader.setDRACOLoader(dracoLoader)  // https://github.com/mrdoob/three.js/issues/26403#issuecomment-2101653981
+  gltfLoader.setKTX2Loader(ktx2Loader)
+  gltfLoader.setMeshoptDecoder(MeshoptDecoder)
+
+  //
+  const modelDir = path.parse(modelFilename).dir + path.sep
   let modelGLTF: GLTF
   return new Promise((resolve, reject) => {
     gltfLoader.parse(
       modelBuffer,
-      '',
+      modelDir, // provide the reference path for relative resources
       async (gltf) => {
         resolve(gltf)
       },
