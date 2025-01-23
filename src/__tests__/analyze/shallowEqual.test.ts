1908beb6365a86bd2f1727e25b7b5ad2334f3bc3
Kevin Ross <kevin.ross@alienfast.com>
2025-01-23 15:02:44 -0600

    add a shallow equal function for comparing props

---
+++ src/__tests__/analyze/shallowEqual.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest'
+
+import { shallowEqual } from '../../analyze/shallowEqual.js'
+
+describe('shallowEqual', () => {
+  it('should work', () => {
+    const obj1 = { name: 'John', age: 33 }
+    const obj2 = { age: 33, name: 'John' }
+    const obj3 = { name: 'John', age: 45 }
+
+    const aobj1 = { name: 'John', age: 33, kids: ['booker', 'archer'] }
+    const aobj2 = { age: 33, kids: ['archer', 'booker'], name: 'John' }
+    const aobj3 = { name: 'John', age: 33, kids: ['bookerx', 'archerx'] }
+
+    expect(shallowEqual(obj1, obj2)).toBe(true)
+    expect(shallowEqual(obj1, obj3)).toBe(false)
+
+    expect(shallowEqual(aobj1, aobj2)).toBe(true)
+    expect(shallowEqual(aobj1, aobj3)).toBe(false)
+  })
+})
---
+++ src/analyze/shallowEqual.ts
@@ -0,0 +1,34 @@
+const compareArrays = (ia1: any[], ia2: any[]) => {
+  if (ia1.length !== ia2.length) {
+    return false
+  }
+
+  const a1 = ia1.sort()
+  const a2 = ia2.sort()
+
+  const result = a1.every((value, index) => a2[index] == value)
+  // console.log("compareArrays sorted", a1, " == ", a2, ": ", result);
+  return result
+}
+
+type Props = Record<string, any>
+
+/**
+ * Performs equality by iterating through keys on an object and returning false
+ * when any key has values which are not equal between the arguments.  If
+ * the values are arrays, it will sort and compare them for equality.
+ */
+export function shallowEqual(o1: Props, o2: Props): boolean {
+  return (
+    Object.keys(o1).length === Object.keys(o2).length &&
+    Object.keys(o1).every((p) => {
+      if (Array.isArray(o1[p]) && Array.isArray(o2[p])) {
+        const result = compareArrays(o1[p], o2[p])
+        // console.log("isArray", o1[p], " == ", o2[p], ": ", result);
+        return result
+      } else {
+        return o1[p] === o2[p]
+      }
+    })
+  )
+}
