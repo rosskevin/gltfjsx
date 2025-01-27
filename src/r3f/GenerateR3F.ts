c9c8785a1ab6c57778b3511a028bb2a6a7f48952
Kevin Ross <kevin.ross@alienfast.com>
2025-01-27 15:24:46 -0600

    added animation types

--- src/r3f/GenerateR3F.ts
+++ src/r3f/GenerateR3F.ts
@@ -1,4 +1,6 @@
+import { Bone, Mesh } from 'three'
 import {
+  FormatCodeSettings,
   FunctionDeclaration,
   InterfaceDeclaration,
   ObjectLiteralExpression,
@@ -6,10 +8,12 @@ import {
   ScriptTarget,
   SourceFile,
   SyntaxKind,
+  ts,
   VariableDeclarationKind,
 } from 'ts-morph'
 
 import { AnalyzedGLTF } from '../analyze/AnalyzedGLTF.js'
+import isVarName from '../analyze/isVarName.js'
 import { JsxOptions } from '../options.js'
 
 export class GeneratedR3F {
@@ -17,8 +21,8 @@ export class GeneratedR3F {
   protected src: SourceFile
   protected gltfInterface!: InterfaceDeclaration
   protected propsInterface!: InterfaceDeclaration
-  protected fn!: FunctionDeclaration
   protected instancesFn: FunctionDeclaration
+  protected fn!: FunctionDeclaration
 
   constructor(
     private a: AnalyzedGLTF,
@@ -52,8 +56,10 @@ export class GeneratedR3F {
     // set constants - load path, draco
     this.setConstants()
 
+    this.setModelGLTFTypes()
+
     // format after manipulation
-    this.src.formatText()
+    this.src.formatText(this.getFormatCodeSettings())
 
     return this.src
   }
@@ -78,12 +84,44 @@ export class GeneratedR3F {
     return result.getFiles()[0].text
   }
 
+  protected getFormatCodeSettings(): FormatCodeSettings {
+    return {
+      semicolons: ts.SemicolonPreference.Remove,
+    }
+  }
+
   protected setConstants() {
     const { draco, modelLoadPath: inModelLoadPath } = this.options
     const modelLoadPath =
       (inModelLoadPath.toLowerCase().startsWith('http') ? '' : '/') + inModelLoadPath
     this.src.getVariableDeclaration('modelLoadPath')?.setInitializer(`'${modelLoadPath}'`)
     this.src.getVariableDeclaration('draco')?.setInitializer(draco ? 'true' : 'false')
+  }
+
+  protected setModelGLTFTypes() {
+    // nodes
+    const meshes: Mesh[] = this.a.getMeshes()
+    const bones: Bone[] = this.a.getBones()
+    const nodes = this.gltfInterface.getProperty('nodes')
+    if (!nodes) throw new Error('gltfInterface nodes not found')
+    nodes.setType(
+      `{ ${[...meshes, ...bones].map(({ name, type }) => (isVarName(name) ? name : `['${name}']`) + ': ' + type).join(', ')} }`,
+    )
+
+    // materials
+    const materials = this.gltfInterface.getProperty('materials')
+    if (!materials) throw new Error('gltfInterface materials not found')
+    materials.setType(
+      `{ ${this.a
+        .getMaterials()
+        .map(({ name, type }) => (isVarName(name) ? name : `['${name}']`) + ': ' + type)
+        .join(', ')} }`,
+    )
+
+    // animations
+    if (!this.a.hasAnimations()) {
+      return
+    }
   }
 
   // FIXME remove this example
@@ -110,6 +148,10 @@ export class GeneratedR3F {
     return this.options.componentName + 'Props'
   }
 
+  protected getModelActionName() {
+    return this.options.componentName + 'Action'
+  }
+
   protected getModelGLTFName() {
     return this.options.componentName + 'GLTF'
   }
@@ -121,18 +163,31 @@ export class GeneratedR3F {
   protected getTemplate() {
     const { componentName, instanceall } = this.options
     const modelGLTFName = this.getModelGLTFName()
+    const modelActionName = this.getModelActionName()
     const modelPropsName = this.getModelPropsName()
     const modelInstancesName = this.getModelInstancesName()
+    const hasAnimations = this.a.hasAnimations()
+
     const template = `
       import { useGLTF } from '@react-three/drei'
       import { GroupProps, MeshProps } from '@react-three/fiber'
       import * as React from 'react'
-      import { Mesh, MeshStandardMaterial, GLTF } from 'three'
+      import { AnimationClip, GLTF, Mesh, MeshStandardMaterial } from 'three'
       import { GLTF } from 'three-stdlib'
+
+      ${
+        hasAnimations
+          ? `
+        type ${modelActionName}Names = ${this.a.gltf.animations.map((clip, i) => `'${clip.name}'`).join(' | ')}
+        interface GLTFAction extends AnimationClip { name: ${modelActionName}Names }
+        `
+          : ''
+      }
 
       interface ${modelGLTFName} extends GLTF {
         nodes: {}
         materials: {}
+        ${hasAnimations ? `animations: ${modelActionName}[]` : ''}
       }
 
       export interface ${modelPropsName} extends GroupProps {}
