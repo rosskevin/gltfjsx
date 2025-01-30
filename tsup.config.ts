70926076726ab4a361048da6a8889f2fad83e9a8
Kevin Ross <kevin.ross@alienfast.com>
2025-01-30 10:00:53 -0600

    setup tsup for build

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+// src/index.ts src/cli.ts --format cjs,esm --dts --clean
+export default defineConfig({
+  entry: ['src/index.ts', 'src/cli.ts'],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ['esm'], // cjs can't be used for top level await
+  minify: false,
+  treeshake: false,
+  splitting: false,
+})
