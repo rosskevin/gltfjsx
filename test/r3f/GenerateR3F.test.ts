b4fe103768c0dbdc0946abc0b8af32db4fb670f4
Kevin Ross <kevin.ross@alienfast.com>
2025-02-03 19:43:39 -0600

    tighten assertions, add exposeProps tests for matcher

--- test/r3f/GenerateR3F.test.ts
+++ test/r3f/GenerateR3F.test.ts
@@ -1,10 +1,12 @@
 import { GLTF } from 'node-three-gltf'
+import { SyntaxKind } from 'ts-morph'
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import {
   AnalyzedGLTF,
   GeneratedR3F,
   GenerateOptions,
+  isMesh,
   loadGLTF,
   resolveModelLoadPath,
 } from '../../src/index.js'
@@ -52,27 +54,30 @@ describe('GenerateR3F', () => {
             expect(g.instancesFn).not.toBeNull()
             expect(g.fn).not.toBeNull()
             expect(g.groupRoot).not.toBeNull()
+            expect(
+              g.groupRoot.getDescendantsOfKind(SyntaxKind.JsxSelfClosingElement).length,
+            ).toEqual(6)
           }
 
           it('should generate', async () => {
             const g = new GeneratedR3F(a, options)
 
-            assertCommon(g)
             const tsx = await g.toTsx()
             console.log(tsx)
             const jsx = await g.toJsx()
+            assertCommon(g)
 
             for (const code of [tsx, jsx]) {
-              expect(tsx).toContain('FOO header')
+              expect(code).toContain('FOO header')
 
               if (type.includes('instanceall')) {
-                expect(tsx).toContain('<Merged')
-                expect(tsx).toContain('const instances = React.useMemo(')
-                expect(tsx).toMatch(/<instances\..*castShadow/)
-                expect(tsx).toMatch(/<instances\..*receiveShadow/)
+                expect(code).toContain('<Merged')
+                expect(code).toContain('const instances = React.useMemo(')
+                expect(code.match(/<instances\..*castShadow/g)?.length).toEqual(6)
+                expect(code.match(/<instances\..*receiveShadow/g)?.length).toEqual(6)
               } else {
-                expect(tsx).toMatch(/castShadow/)
-                expect(tsx).toMatch(/receiveShadow/)
+                expect(code.match(/castShadow/g)?.length).toEqual(6)
+                expect(code.match(/receiveShadow/g)?.length).toEqual(6)
               }
             }
           })
@@ -99,13 +104,14 @@ describe('GenerateR3F', () => {
 
               for (const code of [tsx, jsx]) {
                 if (type.includes('instanceall')) {
-                  expect(tsx).toContain('<Merged')
-                  expect(tsx).toContain('const instances = React.useMemo(')
-                  expect(tsx).toMatch(/<instances\..*castShadow=\{shadows\}/)
-                  expect(tsx).toMatch(/<instances\..*receiveShadow=\{shadows\}/)
+                  expect(code).toContain('<Merged')
+                  expect(code).toContain('const instances = React.useMemo(')
+                  expect(code.match(/<instances/g)?.length).toEqual(6)
+                  expect(code.match(/.*receiveShadow=\{shadows\}/g)?.length).toEqual(6)
+                  expect(code.match(/.*castShadow=\{shadows\}/g)?.length).toEqual(6)
                 } else {
-                  expect(tsx).toMatch(/castShadow=\{shadows\}/)
-                  expect(tsx).toMatch(/receiveShadow=\{shadows\}/)
+                  expect(code.match(/castShadow=\{shadows\}/g)?.length).toEqual(6)
+                  expect(code.match(/receiveShadow=\{shadows\}/g)?.length).toEqual(6)
                 }
               }
             })
@@ -131,15 +137,50 @@ describe('GenerateR3F', () => {
 
               for (const code of [tsx, jsx]) {
                 if (type.includes('instanceall')) {
-                  expect(tsx).toContain('<Merged')
-                  expect(tsx).toContain('const instances = React.useMemo(')
-                  expect(tsx).toMatch(/<instances\..*castShadow=\{shadows\}/)
-                  expect(tsx).toMatch(/<instances\..*receiveShadow /)
+                  expect(code).toContain('<Merged')
+                  expect(code).toContain('const instances = React.useMemo(')
+                  expect(code.match(/<instances\..*castShadow=\{shadows\}/g)?.length).toEqual(6)
+                  expect(code.match(/<instances\..*receiveShadow /g)?.length).toEqual(6)
                 } else {
-                  expect(tsx).toMatch(/castShadow=\{shadows\}/)
-                  expect(tsx).toMatch(/receiveShadow\n/)
+                  expect(code.match(/castShadow=\{shadows\}/g)?.length).toEqual(6)
+                  expect(code.match(/receiveShadow\n/g)?.length).toEqual(6)
                 }
               }
+            })
+
+            describe('matcher', () => {
+              it('should limit matches', async () => {
+                const mo: GenerateOptions = {
+                  ...options,
+                  exposeProps: {
+                    shadows: {
+                      to: 'castShadow',
+                      structure: {
+                        type: 'boolean',
+                        hasQuestionToken: true,
+                      },
+                      matcher: (o) => isMesh(o) && o.name === 'GlassPlastic_low',
+                    },
+                  },
+                }
+                const g = new GeneratedR3F(a, mo)
+                assertCommon(g)
+                const tsx = await g.toTsx()
+                console.log(tsx)
+                const jsx = await g.toJsx()
+
+                for (const code of [tsx, jsx]) {
+                  if (type.includes('instanceall')) {
+                    expect(code).toContain('<Merged')
+                    expect(code).toContain('const instances = React.useMemo(')
+                    expect(code.match(/<instances\..*castShadow=\{shadows\}/g)?.length).toEqual(1)
+                    expect(code.match(/<instances\..*receiveShadow /g)?.length).toEqual(6)
+                  } else {
+                    expect(code.match(/castShadow=\{shadows\}/g)?.length).toEqual(1)
+                    expect(code.match(/receiveShadow\n/g)?.length).toEqual(6)
+                  }
+                }
+              })
             })
           })
         })
