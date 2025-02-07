90d569c8f1a78b15f6e1739206390223a014a01a
Kevin Ross <kevin.ross@alienfast.com>
2025-02-07 11:48:41 -0600

    ncu, don’t lint the readme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,5 @@ export default tseslint.config({
   rules: {
     'no-console': 'off',
   },
-  ignores: ['node_modules', 'dist', 'test/models'],
+  ignores: ['node_modules', 'dist', 'test/models', 'readme.md'],
 })
